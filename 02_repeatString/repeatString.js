4f395a62d7ea4770ab4f4cb85715e1805fcbad79
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-27 09:53:56 -0300

    Add solution 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    const oldString = string;
 
+    if(num > 0){
+        
+        for(let i = 1 ; i < num ; i++)
+            string += oldString;
+
+        return string;
+    }   else if(num == 0){
+        return "";
+    }   else if(num < 0){
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
