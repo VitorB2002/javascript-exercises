bd02b32eb133e6804f4b5bcb42be77fe84092ead
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-27 11:18:33 -0300

    Add solution 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let conversion = (fahrenheit - 32) * (5/9)
 
+    if(conversion%1 == 0){
+      return conversion;
+    }
+
+  return Number(conversion.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let conversion = (celsius * (9/5)) + 32;
 
+    if(conversion%1 == 0){
+      return conversion;
+    }
+
+  return Number(conversion.toFixed(1));
 };
 
 // Do not edit below this line
