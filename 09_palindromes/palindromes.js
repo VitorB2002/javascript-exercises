f6c6207875223dbaeaa52112f1eef224801fcc55
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-29 11:17:19 -0300

    Add palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reverse;
+    let letters = /^[A-Za-z]+$/;
 
+    //Remove whitespace
+    string = string.replace(/\s+/g, '');
+
+    //Removing punctuation
+    for(let i = 0 ; i < string.length ; i++){
+        if(!(string[i].match(letters))){
+            string = string.replace(string[i], "");
+        }
+    }
+
+    reverse = reverseString(string);
+
+    if(reverse.toLowerCase() == string.toLowerCase()){
+        return true;
+    }   else{
+        return false;
+    }
+
+};
+
+const reverseString = function(string) {
+    let reversed = "";
+
+    for(let i = string.length - 1; i >= 0 ; i--){
+        reversed += string[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
