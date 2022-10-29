8fabba97ef381b47f956ea04a24a9cb0bf0edb2c
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-29 11:36:38 -0300

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+    if(a == 1){
+        return 1;
+    }   else if(a == 2){
+        return 1;
+    }   else if(a < 0){
+        return "OOPS";
+    }   else{
+
+        let fibonacci = pastValue = 1;
+        let aux;
+
+            for(let i = 2 ; i < a ; i++){
+                aux = fibonacci;
+                fibonacci += pastValue;
+                pastValue = aux;
+            }
+
+        return fibonacci;
+    }
 };
 
 // Do not edit below this line
